ad31b8a3e18410983083a8efa7d75bfc091f290a
rafipopz98 <itsrealrafi@gmail.com>
2024-03-17 11:25:16 +0530

    Initial Commit

---
+++ clockOS/clockOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockOSApp.swift
+//  clockOS
+//
+//  Created by Muhammad Rafi on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct clockOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
